5d4a4e2b5488ca19be24de59356c38b168788141
zhangshoulei <zhangshoulei@didiglobal.com>
2025-05-30 00:07:02 +0800

    ci assigment3

--- cs106l-assignments/assignment3/class.h
+++ cs106l-assignments/assignment3/class.h
@@ -2,23 +2,6 @@
 #define CLASS_H
 #include <string>
 #include <vector>
-/*
-class matrix{
-  public:
-    matrix(int,int);
-    matrix(){}
-    matrix(int,int,float);
-    void transpose();
-    void from_vector(std::vector<std::vector<float>>);
-    int rows() const;
-    int cols() const; 
-    float& at(int,int);
-  private:
-    int m;
-    int n; 
-    float data_;
-};
-*/
 class student {
     public:
       student(){}
---
+++ matrix/matrix.hpp
@@ -0,0 +1,18 @@
+
+/*
+class matrix{
+  public:
+    matrix(int,int);
+    matrix(){}
+    matrix(int,int,float);
+    void transpose();
+    void from_vector(std::vector<std::vector<float>>);
+    int rows() const;
+    int cols() const; 
+    float& at(int,int);
+  private:
+    int m;
+    int n; 
+    float data_;
+};
+*/
